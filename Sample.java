d30de8b06195375976bb5fa8038446faa1253932
Eduardo-Vargas <eduardo.vargas@gatech.edu>
2017-11-27 22:03:34 -0500

    random push

---
+++ Sample.java
@@ -0,0 +1,40 @@
+public class Sample {
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private String e;
+    private String f;
+
+    Sample(String a, String b, String c, String d, String e, String f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public String getF() {
+        return f;
+    }
+}
